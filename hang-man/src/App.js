24d2d88d5c1a979a78f2b9d2e075c624486d210d
Lydia Lind <lydia.nilsson@gmail.com>
2020-05-25 12:04:21 +0200

    Adding styles.

--- hang-man/src/App.js
+++ hang-man/src/App.js
@@ -5,7 +5,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <h1>Hänga gubbe</h1>
+      <p>
+        Gissa ordet! En bokstav i taget. Om du gissar fel för många gånger kommer du att <strong>hänga</strong> gubben...
+      </p>
+      <img id="gallow" src={require('./images/gallow.jpg')} alt="An empty gallow"/>
+      <p className="bigText">_ _ _ _ _ _ _ _ _</p>
+      <p className="bigText">Antal gissningar: 0</p>
+      <button>a</button>
     </div>
   );
 }
