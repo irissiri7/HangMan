c3e3c12642081ac1a8cc8f4825c06972f329f5a2
Lydia Lind <lydia.nilsson@gmail.com>
2020-05-25 20:15:22 +0200

    removed the components and went back to simpler implementation, waiting for lectures tomorrow to imporove the component implementation

--- hang-man/src/App.js
+++ hang-man/src/App.js
@@ -1,29 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import generateRandomWord from './words'
+import gallow from './images/gallow.jpg';
 
 function App() {
-  const letters = ['a', 'b', 'c', 'd', 'e', 'f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','å','ä','ö']
-  const letterBtns = letters.map(letter => <button className='letter-btn' onClick={handleClick}>{letter}</button>);
+  
   const randomWord = generateRandomWord();
   
-
   return (
     <div className="App">
       <h1>Hänga gubbe</h1>
       <p>
         Gissa ordet! En bokstav i taget. Om du gissar fel för många gånger kommer du att <strong>hänga</strong> gubben...
       </p>
-      <img id="gallow" src={require('./images/gallow.jpg')} alt="An empty gallow"/>
-      <p className="bigText">{makeSecretWordRepresentation(randomWord)}</p>
+      <img id="gallow" src={gallow} alt="An empty gallow"/>
+      <p id="secretWord" className="bigText">{makeSecretWordRepresentation(randomWord)}</p>
       <p className="bigText">Antal gissningar: 0</p>
       <div id="btn-container">
-        {letterBtns}
+        {makeLetterButtons()}
       </div>
       <button id="reset-btn" onClick={handleReset}>Återställ</button>
     </div>
   );
+}
+
+const makeLetterButtons = () => {
+  const letters = ['a', 'b', 'c', 'd', 'e', 'f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','å','ä','ö']
+  const letterBtns = letters.map(letter => <button className='letter-btn' onClick={handleClick}>{letter}</button>);
+  return letterBtns;
 }
 
 const handleClick = e => {
@@ -32,12 +36,15 @@ const handleClick = e => {
   element.style.boxShadow = '0px 5px #000000';
 }
 
-const handleReset = () =>{
+const handleReset = (word) =>{
   let letterButtons = Array.from(document.getElementsByClassName('letter-btn'));
   letterButtons.map(letterButton => {
     letterButton.style.backgroundColor = '#ffc107';
     letterButton.style.boxShadow = '0px 5px #ff9800';
   })
+
+  word = generateRandomWord();
+  document.getElementById('secretWord').innerText = makeSecretWordRepresentation(word);
 }
 
 const makeSecretWordRepresentation = (word) => {
