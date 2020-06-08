a6f21ff922867e54e35d87ab56176f279d41a9c7
Lydia Lind <lydia.nilsson@gmail.com>
2020-06-08 10:45:45 +0200

    destruct props

--- hang-man/src/components/ResetButton.js
+++ hang-man/src/components/ResetButton.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import generateRandomWord from '../words';
 
-const ResetButton = ({setRandomWord, setGuessedLetters, setCount}) => {
+const ResetButton = ({setRandomWord, setGuessedLetters, setCount, id}) => {
     const handleReset = () => {
         let letterButtons = Array.from(document.getElementsByClassName('letter-btn-inactive'));
         letterButtons.map(letterButton => {
@@ -13,7 +13,7 @@ const ResetButton = ({setRandomWord, setGuessedLetters, setCount}) => {
         setCount(0);
     }
 
-    return <button id={props.id} onClick={handleReset}>Återställ</button>
+    return <button id={id} onClick={handleReset}>Återställ</button>
 }
 
 export default ResetButton;
