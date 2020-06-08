83e0a89effcea30f02d4c82b321e6b9b7285eb64
Lydia Lind <lydia.nilsson@gmail.com>
2020-06-08 10:44:29 +0200

    Fixe function -> const and destructing props
    Forgott this one first time around

--- hang-man/src/components/ResetButton.js
+++ hang-man/src/components/ResetButton.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import generateRandomWord from '../words';
 
-export default function ResetButton(props){
+const ResetButton = ({setRandomWord, setGuessedLetters, setCount}) => {
     const handleReset = () => {
         let letterButtons = Array.from(document.getElementsByClassName('letter-btn-inactive'));
         letterButtons.map(letterButton => {
         letterButton.classList.remove('letter-btn-inactive');
         letterButton.disabled = false;
         })
-        props.setRandomWord(generateRandomWord());
-        props.setGuessedLetters([]);
-        props.setCount(0);
+        setRandomWord(generateRandomWord());
+        setGuessedLetters([]);
+        setCount(0);
     }
 
     return <button id={props.id} onClick={handleReset}>Återställ</button>
 }
+
+export default ResetButton;
