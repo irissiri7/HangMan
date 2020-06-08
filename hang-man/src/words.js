13759d5fa3f244009c25c0777abdd6e596f2c6e1
Lydia Lind <lydia.nilsson@gmail.com>
2020-06-08 13:54:38 +0200

    Making more words available

--- hang-man/src/words.js
+++ hang-man/src/words.js
@@ -1,6 +1,6 @@
 const generateRandomWord = () => {
-    // const randomWords = ["katt", "skruvmejsel", "vitkål", "termos", "glasögon"];
-    const randomWords = ["katt"];
+    const randomWords = ["katt", "skruvmejsel", "vitkål", "termos", "glasögon"];
+    // const randomWords = ["katt"];
     const randomWord = randomWords[Math.floor(Math.random() * randomWords.length)];
     return randomWord;
 }
