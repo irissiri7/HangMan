e781bb0b1098cb76623252716b7c5baca85f9a6d
Lydia Lind <lydia.nilsson@gmail.com>
2020-06-08 11:56:01 +0200

    implement ability to win and lose and update the image accordingly

--- hang-man/src/App.js
+++ hang-man/src/App.js
@@ -1,7 +1,6 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import generateRandomWord from './words';
-import gallow from './images/gallow.jpg';
 import KeyBoard from './components/KeyBoard';
 import Header from './components/Header';
 import Button from './components/Button';
@@ -9,26 +8,41 @@ import Button from './components/Button';
 const App = ({letters}) => {
   
   //Hooks
+  let [image, setImage] = useState(1)
   let [count, setCount] = useState(0);
   let [randomWord, setRandomWord] = useState(generateRandomWord());
   let [guessedLetters, setGuessedLetters] = useState([]);
 
-  
+  useEffect(()=>{
+    if(makeSecretWordRepresentation() == randomWord){
+      alert('you win!')
+      handleReset();
+    }
+    else if(count >= 6){
+      alert('you loose');
+      handleReset();
+    }
+  },[image])
 
 
   const handleClick = e => {
     const element = e.target;
+    const letter = element.innerText;
+    if(!randomWord.split('').includes(letter)){
+      setCount(prev => prev +1);
+      setImage(prev => prev +1);
+
+    }
     setGuessedLetters(guessedLetters = [...guessedLetters, element.innerText]);
     element.classList.add('letter-btn-inactive');
     element.disabled = true;
-    setCount(++count);
   }
 
   const makeSecretWordRepresentation = () => {
     let secretWord = '';
     randomWord.split('').map((letter, index) => {
       if(randomWord[index] == letter && guessedLetters.includes(letter)){
-        secretWord += letter + ' ';
+        secretWord += letter;
       }
       else{
         secretWord += '_ ';
@@ -43,6 +57,7 @@ const App = ({letters}) => {
     letterButton.classList.remove('letter-btn-inactive');
     letterButton.disabled = false;
     })
+    setImage(1);
     setRandomWord(generateRandomWord());
     setGuessedLetters([]);
     setCount(0);
@@ -52,9 +67,9 @@ const App = ({letters}) => {
   return (
     <div className="App">
       <Header/>
-      <img id="gallow" src={gallow} alt="An empty gallow"/>
+      <img id="gallow" src={require(`./images/gallow${image}.jpg`)} alt="An empty gallow"/>
       <p id="secretWord" className="bigText">{makeSecretWordRepresentation()}</p>
-      <p className="bigText">Antal gissningar: {count}</p>
+      <p className="bigText">Antal fel gissningar: {count}</p>
       <div id="btn-container">
         <KeyBoard className = 'letter-btn-default' letters={letters} handleClick = {handleClick}/>
       </div>
